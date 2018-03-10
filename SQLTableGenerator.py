66a5ba7d229f7c2b60cd5e10194fe48bf20164b5
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-03-10 16:45:47 -0500

    Add files via upload

--- SQLTableGenerator.py
+++ SQLTableGenerator.py
@@ -2,26 +2,48 @@
 #Started: February 19, 2018
 #Finished: ?
 
+class TableEntry:
+	def __init__(self,name,fieldType,fieldSize,isNull,isUnique):
+		self.name = name
+		self.type = fieldType.upper()
+		self.size = fieldSize
+		self.null = isNull
+		self.unique = isUnique
+		self.entries = []
+
+	def toString(self):
+		if self.type != "INT":
+			return ''.join([self.name," ",self.type, "(",self.size,") ", self.null])
+		else:
+			return ''.join([self.name," ",self.type," ",self.null])
+
+	def parse(self,fileName):
+		newFilePtr = (fileName,"r")
+
+
 #**********
 #*  Main  *
 #**********
 
 entryDict = {}
+primaryKey = ""
 tableName = raw_input("Enter the name of your table: ")
 fieldNumber = input("Enter the number of fields you wish your table to have: ")
 print ""
 
 for i in range(0,fieldNumber):
-	catList = [None]*4
 	name = raw_input("Name of field?: ")
-	catList[0] = name
 	fieldType = raw_input("Field type?: ")
-	catList[1] = fieldType
 	fieldSize = raw_input("Field size?: ")
-	catList[2] = fieldSize
-	isNull = raw_input("Can it be null?: ")
-	catList[3] = isNull	
-	entryDict[i] = catList
+	isNull = raw_input("Can it be null?[yes/no]: ")
+	if isNull == "yes":
+		isNull = "NOT NULL"
+	isPrimary = raw_input("Is this the primary key?: ")
+	if isPrimary == "yes":
+		primaryKey = name
+	fieldObj = TableEntry(name,fieldType,fieldSize,isNull,isPrimary)
+	myFile = raw_input("Enter the file you want to parse: ")
+	entryDict[i] = fieldObj
 
 outputFile = "NewTable-" + tableName + ".sql"
 filePtr = open(outputFile,"w+")
@@ -30,8 +52,14 @@ filePtr.write(string)
 for j in range(0,fieldNumber):
 	if j > 0:
 		filePtr.write(",")
-	arr = entryDict[j]
-	string = ''.join([arr[0]," ", arr[1], "(", arr[2],") ", arr[3]])
+	thisObj = entryDict[j]
+	string = thisObj.toString()
 	filePtr.write(string)
-filePtr.write(");")
+if primaryKey != "":
+	tempStr = ''.join([",PRIMARY KEY(",primaryKey,")"])
+	filePtr.write(tempStr)
+filePtr.write(");\n")
+
+#fieldNumber = input("How many tuples would you like?: ")
+
 filePtr.close()
